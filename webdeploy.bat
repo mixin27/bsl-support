6118424d122e2d0b926627ab136a72def5ad9c97
mixin27 <kyawzayartun.dev@gmail.com>
2024-12-03 10:35:34 +0630

    fixed deployment folder to docs

--- webdeploy.bat
+++ webdeploy.bat
@@ -8,11 +8,11 @@ echo "Generating the web folder..."
 CALL flutter create . --platform web
 
 echo "Building for web..."
-CALL flutter build web --base-href /bsl-support-site/ --release
+CALL flutter build web --base-href /bsl-support/ --release
 
-echo "Copying build to site..."
-CALL del .\site /S /Q /F
-CALL mkdir .\site
-CALL xcopy /s /i .\build\web .\site
+echo "Copying build to docs..."
+CALL del .\docs /S /Q /F
+CALL mkdir .\docs
+CALL xcopy /s /i .\build\web .\docs
 
-echo "✅ Finished copy build/web/* to ./site/*"
+echo "✅ Finished copy build/web/* to ./docs/*"
